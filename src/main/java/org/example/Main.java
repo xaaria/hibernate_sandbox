9d3ab34634af2683ec133994ddaf6f3cfff5acc4
okkop <okkop@invalid.com>
2024-09-11 16:06:36 +0300

    Add some example code. Person with apartments.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,15 @@
 package org.example;
 
+import org.example.entity.House;
+import org.example.entity.Person;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.Random;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -14,7 +20,28 @@ public class Main {
 
         System.out.printf("Hello and welcome!");
         Main.setUp();
+        System.out.println("***");
 
+        Session ses = sessionFactory.openSession();
+        Transaction t = ses.beginTransaction();
+
+        Person p = new Person();
+        p.setFirstname(getRandomFirstname());
+        p.setLastname(getRandomLastname());
+        p.setSex(2);
+        // Add apartment for person
+        House h = new House("Kujakatu", 31);
+        House h2 = new House("Hämeenkatu", 93);
+
+        ses.persist(h);
+        ses.persist(h2);
+
+        p.getHouses().add(h);
+        p.getHouses().add(h2);
+
+        ses.persist(p);
+
+        t.commit();
 
     }
 
@@ -31,10 +58,23 @@ public class Main {
                             .buildSessionFactory();
         }
         catch (Exception e) {
+
+            System.out.println( e );
+
             // The registry would be destroyed by the SessionFactory, but we
             // had trouble building the SessionFactory so destroy it manually.
             StandardServiceRegistryBuilder.destroy(registry);
         }
+    }
+
+    private static String getRandomFirstname() {
+        String[] names = {"Seppo", "Matti", "Teppo", "Martti", "Sami", "Marko", "Antti"};
+        return names[ new Random().nextInt(names.length) ];
+    }
+
+    private static String getRandomLastname() {
+        String[] names = {"Virtanen", "Vuorinen", "Mäkinen", "Jokinen", "Korhonen", "Rantanen"};
+        return names[ new Random().nextInt(names.length) ];
     }
 
 }
